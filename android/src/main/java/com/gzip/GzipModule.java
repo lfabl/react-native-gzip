61e70700c0fb2069989d0325a192732256e8dbda
lfabl <met2net3@gmail.com>
2023-07-02 00:41:37 +0300

    TEST: 3

--- android/src/main/java/com/gzip/GzipModule.java
+++ android/src/main/java/com/gzip/GzipModule.java
@@ -18,6 +18,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+import java.io.UnsupportedEncodingException;
 
 import com.facebook.react.bridge.ReadableArray;
 
